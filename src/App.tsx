eeafc147b0a9093e1dfaee8e75a58446fc421ce4
wexire <peter.pol.code@gmail.com>
2023-01-12 18:59:28 +0200

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ArticlePage from "./pages/ArticlePage";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
-  return <Container>App</Container>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/:id" element={<ArticlePage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/ArticlePage.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const ArticlePage = () => {
+  return <Container>ArticlePage</Container>;
+};
+
+export default ArticlePage;
